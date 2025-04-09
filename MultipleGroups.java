bc888c9b8937423245b501e87353377827672307
gHUME15 <Grayson.hume@stu.daviess.kyschools.us>
2025-04-09 13:29:40 +0000

    Don't like

--- MultipleGroups.java
+++ MultipleGroups.java
@@ -1,1 +1,5 @@
+public class MutipleGroups implements NumberGroup
 
+For(int i = 0;i<0;i++)
+
+return false;
--- Range.java
+++ Range.java
@@ -1,1 +1,15 @@
+public int Range
+
+privite int rangeMin;
+privite int rangMax ;
+
+NumberGroup range1 = (-2,2);
+
+public range(int x,int y)
+rangeMin = x;
+rangMax = y;
+
+
+
+
 
